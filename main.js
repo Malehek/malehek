09de139fc6f2c1dd63e02ba2745b74a8fda5b0fd
Malehek <yikmalehek23@gmail.com>
2025-04-02 15:12:51 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// Importing Typed.js library
+
+var typed = new Typed('.text', {
+    strings: ['<i>Frontend Developer</i>', '<i>Data Annotator</i>', '<i>Virtual Assistant</i>', '<i>Web Designer</i>'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true,
+  });
